905d4ba3e79a4cb31418746c5219f224c472d403
nmakhamatov <zeropulse8@gmail.com>
2025-06-16 00:47:50 +0600

    adding items feature implemented

--- lib/screens/grocery_list.dart
+++ lib/screens/grocery_list.dart
@@ -1,53 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/categories.dart';
-import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/screens/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+
+    if (newItem == null) {
+      return; // User cancelled the item creation
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+
+  @override
   Widget build(BuildContext context) {
+    const emptyPage = Center(
+      child: Text(
+        'No items added yet!',
+        style: TextStyle(fontSize: 20, color: Colors.grey),
+      ),
+    );
+
+    var content;
+
+    if (!_groceryItems.isEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text(_groceryItems[index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: _groceryItems[index].category.color,
+          ),
+          trailing: Text(
+            _groceryItems[index].quantity.toString(),
+          ),
+        ),
+      );
+    } else {
+      content = emptyPage;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _addItem
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) {
-          final grocery_item = groceryItems.elementAt(index);
-          final category = grocery_item.category;
-
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 24.0, left: 16.0, right: 16.0),
-            child: Row(
-              children: [
-                Container(
-                  width: 24, // Ширина цветного квадрата
-                  height: 24, // Высота цветного квадрата
-                  decoration: BoxDecoration(
-                    color: category.color, // Цвет категории
-                    shape: BoxShape.rectangle, // Форма круга
-                  ),
-                ),
-
-                const SizedBox(width: 20), // Отступ между цветом и текстом
-                Text(
-                  grocery_item.name, // Название категории
-                  style: const TextStyle(fontSize: 16),
-                ),
-
-                const Spacer(), // Добавляет пустое пространство
-                Text(
-                  '${grocery_item.quantity}', // Количество элементов
-                  style: const TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-          );
-        },
-          
-    )
+      body: content,
     );
   }
-
 }
